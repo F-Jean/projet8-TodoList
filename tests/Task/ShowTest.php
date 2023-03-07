87b304551c7e7b63b5baa09fb3347ac2f566229b
F-Jean <jean.courtot@gmail.com>
2023-03-07 21:47:03 +0100

    documentation: ajout des derniers livrables, diagrammes + code coverage PHPUnit

--- tests/Task/ShowTest.php
+++ tests/Task/ShowTest.php
@@ -14,7 +14,7 @@ class ShowTest extends WebTestCase
     /**
      * @test
      */
-    public function taskShouldBeDisplayed(): void
+    public function taskTodoShouldBeDisplayed(): void
     {
         $client = static::createClient();
 
@@ -38,5 +38,32 @@ class ShowTest extends WebTestCase
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
         /* On test l'affichage des 9 tasks max par page due à la pagination */
         $this->assertCount(9, $crawler->filter('.card'));
+    }
+
+    /**
+     * @test
+     */
+    public function taskDoneShouldBeDisplayed(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get("doctrine.orm.entity_manager");
+
+        /** @var User $user */
+        $user = $entityManager->getRepository(User::class)->findOneBy([]);
+        $client->loginUser($user);
+
+        /* On récupère le router pour générer directement une url
+        car plus tard l'url peut évolué donc on ne veut pas l'écrire en dure */
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get("router");
+
+        $crawler = $client->request(
+            Request::METHOD_GET,
+            $urlGenerator->generate("task_list_done")
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
     }
 }
